69efcc79a19e8316ba8d23918f521eb1ac444d19
Tony Tran <tonytran504@gmail.com>
2022-05-08 22:02:38 -0500

    added readme, video link, repo link

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # team-profile-generator
+
+I've create a team profile generator for users that need a quick way to build a team profile with different roles and information for each specifc role. This
+
+A user will be prompted of which type of employee they'd like to create, followed by inputting relevant information. This
+
+When a user is done, an HTML file with be generated. 
+
+REPO:https://github.com/TonyT504/team-profile-generator
+VIDEO: https://drive.google.com/file/d/1dET3BYZEaCIyKHYXZxTzBZ36gLaEPSDm/view 
--- src/template.js
+++ src/template.js
@@ -1,61 +1,61 @@
 
 
 const generateBody = (receiveUserData) => {
-    
+
     const employeeCard = receiveUserData.map(index => {
         if (index.officeNumber) {
             const { name, email, id, officeNumber } = index;
             return `
-                <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
-                    <div class="bg-blue-500 text-slate-100 text-xl text-center rounded-t" id="employee-information">
-                        <div id="employee-name">${name}</div>
-                        <div id="employee-title">Manager</div>
-                    </div>
-                    <div class="p-5 rounded-b" id="container">
-                        <div id="employee-details">
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">ID #: ${id}</div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-phone">Office #: ${officeNumber}</div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
-                        </div>
+            <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
+                <div class="bg-green-200 text-slate text-xl text-center rounded-t" id="employee-information">
+                    <div id="employee-name">${name}</div>
+                    <div id="employee-title">Manager</div>
+                </div>
+                <div class="p-5 rounded-b" id="container">
+                    <div id="employee-details">
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">ID #: ${id}</div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-phone">Office #: ${officeNumber}</div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
                     </div>
                 </div>
-                `;
+            </div>
+            `;
         }
         if (index.github) {
             const { name, email, id, github } = index;
             return `
-                <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
-                    <div class="bg-blue-500 text-slate-100 text-xl text-center rounded-t" id="employee-information">
-                        <div id="employee-name">${name}</div>
-                        <div id="employee-title">Engineer</div>
-                    </div>
-                    <div class="p-5 rounded-b" id="container">
-                        <div id="employee-details">
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">ID #: ${id}</div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-github">GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
-                        </div>
+            <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
+                <div class="bg-green-200 text-slate text-xl text-center rounded-t" id="employee-information">
+                    <div id="employee-name">${name}</div>
+                    <div id="employee-title">Engineer</div>
+                </div>
+                <div class="p-5 rounded-b" id="container">
+                    <div id="employee-details">
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">ID #: ${id}</div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-github">GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
                     </div>
                 </div>
-                `;
+            </div>
+            `;
         }
         if (index.school) {
             const { name, email, id, school } = index;
             return `
-                <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
-                    <div class="bg-blue-500 text-slate-100 text-xl text-center rounded-t" id="employee-information">
-                        <div id="employee-name">${name}</div>
-                        <div id="employee-title">Intern</div>
-                    </div>
-                    <div class="p-5 rounded-b" id="container">
-                        <div id="employee-details">
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">School: ${id}</div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-school">School: ${school}</div>
-                            <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
-                        </div>
+            <div class="flex flex-col bg-slate-200 m-5 drop-shadow-lg rounded-b" id="profile">
+                <div class="bg-green-200 text-slate text-xl text-center rounded-t" id="employee-information">
+                    <div id="employee-name">${name}</div>
+                    <div id="employee-title">Intern</div>
+                </div>
+                <div class="p-5 rounded-b" id="container">
+                    <div id="employee-details">
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="ID#">School: ${id}</div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-school">School: ${school}</div>
+                        <div class="bg-slate-100 m-1 p-2 drop-shadow-lg" id="employee-email">E-mail: <a href="mailto:${email}">${email}</a></div>
                     </div>
                 </div>
-                `;
+            </div>
+            `;
         }
     });
     return employeeCard.join('');
@@ -75,7 +75,7 @@ const generateTemplate = (receiveUserData) => {
 </head>
 <body class="relative min-h-screen">
     <header>
-        <h1 class="bg-red-900 text-slate-100 text-center text-4xl p-5">My Team's Portfolios</h1>
+        <h1 class="bg-green-200 text-slate text-center text-4xl p-5">Team Members</h1>
     </header>
     <main class="flex flex-wrap justify-center">
         ${generateBody(receiveUserData)}
